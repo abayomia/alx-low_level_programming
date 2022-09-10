205a4c3d5653c23c420641c2a2fe4e44b34b8343
abayomia <abayomiadewale@gmail.com>
2022-09-10 07:27:08 -0700

    Check task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase, followed by a new line
+ * using only putchar()
+ * Return: 0 upon successful run
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
