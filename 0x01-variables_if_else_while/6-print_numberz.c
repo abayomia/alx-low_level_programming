14689c784f7ffe26d90f7771a51dd7482af58eaa
abayomia <abayomiadewale@gmail.com>
2022-09-10 09:16:57 -0700

    Check answers 5 to 8

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single-digit numbers of base 10 from 0
+ * followed by a new line, using putchar()
+ * Return: 0 upon successful completion
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n += 0; n < 10; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints lowercase alphabet in reverse followed by new line
+ * using putchar()
+ * Return: 0 upon successful run
+ */
+
+int main(void)
+{
+	char ch;
+
+	for(ch = 'z'; ch <= 'a'; ch--)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
