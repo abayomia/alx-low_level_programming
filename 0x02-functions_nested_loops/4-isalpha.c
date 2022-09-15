7306a7c83be670a31a82b6e0ee2dfc13c9f6e7c5
abayomia <abayomiadewale@gmail.com>
2022-09-14 19:36:42 -0700

    Fixed answer 4, added character error

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,10 +5,7 @@
  * @c: character to check
  * Return: 1 if c is in alphabet, 0 if otherwise
  */
-int _isalpha(int c);
+int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-		return (1);
-	else
-		return (0);
+       return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
